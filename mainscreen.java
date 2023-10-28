88ed79b1c4d997d0545416f6c80794d72892ea88
Alphatechsoft <149204419+Alphatechsoft@users.noreply.github.com>
2023-10-28 16:25:21 -0400

    Create mainscreen.java

---
+++ mainscreen.java
@@ -0,0 +1,12 @@
+import java.time.*;
+import java.time.format.*;
+import javax.swing.*;
+
+//Make the CoffeeOS home screen
+
+public class mainscreen {
+  public getTime() {
+    LocalDateTime localTime = LocalDateTime.now();
+    DateTimeFormatter localTimeFormatted = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+  }
+}
