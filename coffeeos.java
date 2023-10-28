8564b510e6654b6b3990532413dc4af2e5d7d9d7
Alphatechsoft <149204419+Alphatechsoft@users.noreply.github.com>
2023-10-28 16:05:03 -0400

    Update and rename javaos.java to coffeeos.java

--- coffeeos.java
+++ coffeeos.java
@@ -1,10 +1,10 @@
-/* JavaOS by Alphatechsoft
+/* CoffeeOS by Alphatechsoft
 Copyright 2023 */
 
 import javax.swing.*;
 
 class javaos {
   private static void initWindow() {
-    JFrame javaoswindow = new JFrame("JavaOS V. 1.0.0");
+    JFrame CoffeeOSWindow = new JFrame("CoffeeOS V. 1.0.0");
       }
 }
