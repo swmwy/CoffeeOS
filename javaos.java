04c46dc0051d69ffca4ae5d0bf9af7a2bce24e29
Alphatechsoft <149204419+Alphatechsoft@users.noreply.github.com>
2023-10-28 15:49:23 -0400

    Update javaos.java

--- javaos.java
+++ javaos.java
@@ -1,3 +1,10 @@
+/* JavaOS by Alphatechsoft
+Copyright 2023 */
+
+import javax.swing.*;
+
 class javaos {
-  
+  private static void initWindow() {
+    JFrame javaoswindow = new JFrame("JavaOS V. 1.0.0")
+      }
 }
