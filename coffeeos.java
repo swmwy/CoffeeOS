6c9ded583582d725af8893df54910eb33af7ec1b
Alphatechsoft <149204419+Alphatechsoft@users.noreply.github.com>
2023-10-29 15:01:27 -0400

    Update coffeeos.java

--- coffeeos.java
+++ coffeeos.java
@@ -6,5 +6,6 @@ import javax.swing.*;
 class coffeeos {
   JFrame CoffeeOSWindow = new JFrame("CoffeeOS V. 1.0.0 (Alpha)");
   CoffeeOSWindow.setSize(1920,1080);
-  
+  CoffeeOSWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  CoffeeOSWindow.setVisible(true);
 }
