58ef371f5dba2bc315e8c0a188d8176197970f24
AtlasTechSoft <149204419+AtlasTechSoft@users.noreply.github.com>
2023-11-02 17:30:43 -0400

    Update coffeeos.java

--- coffeeos.java
+++ coffeeos.java
@@ -1,11 +1,7 @@
-/* CoffeeOS by Alphatechsoft
+/* CoffeeOS by AtlasTechSoft
 Copyright 2023 */
 
 import javax.swing.*;
 
 class coffeeos {
-  JFrame CoffeeOSWindow = new JFrame("CoffeeOS V. 1.0.0 (Alpha)");
-  CoffeeOSWindow.setSize(1920,1080);
-  CoffeeOSWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  CoffeeOSWindow.setVisible(true);
 }
